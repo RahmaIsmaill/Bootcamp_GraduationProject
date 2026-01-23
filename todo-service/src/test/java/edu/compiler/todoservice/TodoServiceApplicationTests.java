085b1a60635692da753887269af83915aa231170
RahmaIsmaill <rahmaismail2010@gmail.com>
2026-01-23 02:27:34 +0200

    first

---
+++ todo-service/src/test/java/edu/compiler/todoservice/TodoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.compiler.todoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
